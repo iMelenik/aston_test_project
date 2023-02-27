0548d06513ea7043a48dd604f391f935575eae6a
iMelenik <dteamaker@gmail.com>
2023-02-27 13:28:19 +0300

    user admin create

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.contrib import admin
+from .models import UserProfile
+
+User = settings.AUTH_USER_MODEL
+
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    list_display_links = ['user']
+
+    # class UserInLine(admin.TabularInline):
+    #     model = User
+    #     extra = 0
+    #
+    # inlines = [UserInLine]
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
