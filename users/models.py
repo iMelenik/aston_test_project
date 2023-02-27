6dc0c0a262bbb0baf67897dcbbabcd29ffa53581
iMelenik <dteamaker@gmail.com>
2023-02-27 11:26:24 +0300

    User model updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from wallets.models import Wallet
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def get_all_user_wallets(self):
+        pass
+
+    def get_wallets_number(self):
+        pass
