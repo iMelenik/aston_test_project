a54bb99e4b25f0e2bf60541076b2647c16a6b7d8
iMelenik <dteamaker@gmail.com>
2023-02-27 13:09:59 +0300

    user model fix, migrations

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
-
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
