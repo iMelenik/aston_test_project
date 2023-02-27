d610d7cd3e24fcd5d97e1d7196f0bc7202ab3c98
iMelenik <dteamaker@gmail.com>
2023-02-27 13:31:24 +0300

    wallet admin create

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Wallet
+
+
 # Register your models here.
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type', 'balance', 'currency']
+
+
+admin.site.register(Wallet, WalletAdmin)
