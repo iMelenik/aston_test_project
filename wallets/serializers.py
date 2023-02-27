229434053a103f89d908bb012ceba05583e02680
iMelenik <dteamaker@gmail.com>
2023-02-27 15:46:38 +0300

    wallet serializer readonly fields set

--- wallets/serializers.py
+++ wallets/serializers.py
@@ -5,8 +5,9 @@ from .models import Wallet
 
 
 class WalletSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.HyperlinkedRelatedField(view_name='users:detail', queryset=UserProfile.objects.all())
+    user = serializers.HyperlinkedRelatedField(view_name='users:detail', read_only=True)
 
     class Meta:
         model = Wallet
         fields = ['id', 'name', 'user', 'type', 'currency', 'balance', 'created_on', 'modified_on']
+        read_only_fields = ['balance']
