49c92b7008ad1d43c86e3be2b030c0bf068240c3
iMelenik <dteamaker@gmail.com>
2023-03-02 10:45:24 +0300

    user testcases login logout

--- users/tests.py
+++ users/tests.py
@@ -1,20 +1,15 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 from users.models import UserProfile
+
 User = get_user_model()
 
 
 class UserTestCase(APITestCase):
-    """
-    1) проверка создания User и UserProfile при регистрации
-    2) проверка генерации токена при логине
-    3) проверка удаления токена при логауте
-    """
-
     def test_registration(self):
         """
         GET: method not allowed
@@ -26,9 +21,9 @@ class UserTestCase(APITestCase):
             'password': 'pass',
             'password2': 'pass',
             'email': 'tt@tt.ru',
-                }
-        response = self.client.post(url, data, format='json')
+        }
 
+        response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         self.assertEqual(User.objects.count(), 1)
@@ -45,7 +40,56 @@ class UserTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_login(self):
-        pass
+        """
+        GET: method not allowed
+        POST: sending username and password returns token
+        """
+        url = reverse('users:login')
+        data = {
+            'username': 'name',
+            'password': 'pass',
+        }
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        user = User.objects.create_user(
+            username='name',
+            password='pass',
+            email='tt@tt.ru',
+        )
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['token'], Token.objects.get(user=user).key)
 
     def test_logout(self):
-        pass
+        """
+        ALL: requires authentication
+        GET: method not allowed
+        POST: successfully logout, token deleted
+        """
+        url = reverse('users:logout')
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response = self.client.post(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        user = User.objects.create_user(
+            username='name',
+            password='pass',
+            email='tt@tt.ru',
+        )
+        token = Token.objects.create(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        response = self.client.post(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, 'Successfully logout.')
+
+        with self.assertRaises(Token.DoesNotExist):
+            Token.objects.get(user=user)
